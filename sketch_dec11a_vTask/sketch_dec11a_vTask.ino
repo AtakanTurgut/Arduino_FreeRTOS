d238b82d82628daf0eb2ec64849c4a0ba555b737
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2023-12-11 23:43:08 +0300

    11-12-2023

--- sketch_dec11a_vTask/sketch_dec11a_vTask.ino
+++ sketch_dec11a_vTask/sketch_dec11a_vTask.ino
@@ -4,44 +4,50 @@
 void vTask1(void *pvParameters);
 void vTask2(void *pvParameters);
 
-void setup() {
+void setup()
+{
   xTaskCreate(
-    vTask1,
-    "Task 1",
-    100,
-    NULL,
-    1,
-    NULL);
+      vTask1,
+      "Task 1",
+      100,
+      NULL,
+      1,
+      NULL);
 
   xTaskCreate(
-    vTask2,
-    "Task 2",
-    100,
-    NULL,
-    2,
-    NULL);
+      vTask2,
+      "Task 2",
+      100,
+      NULL,
+      2,
+      NULL);
 
   vTaskStartScheduler();
 }
 
-void loop() {
+void loop()
+{
 }
 
-void vTask1(void *pvParameters) {
+void vTask1(void *pvParameters)
+{
   (void)pvParameters;
   const char *pcTaskName = "Task1 calisti.";
   Serial.begin(9600);
-  for (;;) {
+  for (;;)
+  {
     Serial.println(pcTaskName);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
-void vTask2(void *pvParameters) {
+void vTask2(void *pvParameters)
+{
   (void)pvParameters;
   const char *pcTaskName = "Task2 calisti.";
   Serial.begin(9600);
-  for (;;) {
+  for (;;)
+  {
     Serial.println(pcTaskName);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
